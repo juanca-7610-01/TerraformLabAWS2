a5f19954c2cedc04977f0b8ed7cf855896688b3c
Juan Pereira <juanca_7610_01@hotmail.com>
2025-02-11 09:20:10 -0500

    mostrar url de s3

---
+++ main.tf
@@ -0,0 +1,21 @@
+provider "aws" {
+  region = "us-east-1"
+  profile = "default"
+}
+
+resource "aws_s3_bucket" "mi_primer_bucket" {
+  bucket = "mi-bucket-de-pruebas-terraform-123456"
+  acl    = "private"
+}
+
+output "s3_bucket_name" {
+  value = aws_s3_bucket.mi_primer_bucket.bucket
+}
+
+output "bucket_arn" {
+  value = aws_s3_bucket.mi_primer_bucket.arn
+}
+
+output "bucket_url"{
+    value = aws_s3_bucket.mi_primer_bucket.bucket_domain_name
+}
